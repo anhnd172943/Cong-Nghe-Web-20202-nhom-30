69a3dbc59b73a42936d18a57f0d542ef1394277a
anhnd172943 <72445400+anhnd172943@users.noreply.github.com>
2021-03-25 16:59:07 +0700

    Lab 00 and 01

---
+++ Lab01/ex2.php
@@ -0,0 +1,67 @@
+<html>
+    <body>
+        <table style="width: 700px" border="1">
+            <tr>
+                    <td style="width: 500px"><p>
+                        This is my personal page
+                        </p></td>
+                            <td>
+                                <img src="https://i.pinimg.com/236x/2b/59/0f/2b590ff8048658f0943e054dfce1a3c1.jpg"
+                                     height="200"
+                                     width="200"/>
+                            </td>
+                </tr>
+        </table>
+        <table border="1" style="width: 700px">
+                         <tr>
+                    <td style="word-wrap: normal"><p style="font-weight: bold">
+                        Education process
+                        </p></td>
+                    <td style="width: 75%">Đại học Bách Khoa Hà Nội</td>
+                </tr>
+                <tr>
+                    <td><p style="font-weight: bold">
+                        Working process
+                        </p></td>
+                    <td>None</td>
+                </tr>
+                <tr>
+                    <td><p style="font-weight: bold">
+                        Society activities
+                        </p></td>
+                    <td>Đội tình nguyện</td>
+                </tr>
+                <tr>
+                    <td><p style="font-weight: bold">
+                        Skills
+                        </p></td>
+                    <td>Lập trình OOP</td>
+                </tr>
+                <tr>
+                    <td><p style="font-weight: bold">
+                        Experiences
+                        </p></td>
+                    <td>None</td>
+                </tr>
+                <tr>
+                    <td><p style="font-weight: bold">
+                        Certifications
+                        </p></td>
+                    <td>None</td>
+                </tr>
+                <tr>
+                    <td><p style="font-weight: bold">
+                        Hobby
+                        </p></td>
+                    <td>Game, Music</td>
+                </tr>
+                <tr>
+                    <td><p style="font-weight: bold">
+                        Desire
+                        </p></td>
+                    <td>Masterchef</td>
+                </tr>
+        </table>
+
+    </body>
+</html>
---
+++ Lab01/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <h1> 1.1 </h1>
+        <p> HTML Example </p>
+        <a href="https://www.google.com.vn/"> Google </a>
+        
+        
+        <h1> 1.2 </h1>
+        
+        <h2> 1.2.1 </h2>
+        <h1> This is a heading </h1>
+        <!-- Create a line -->
+        <hr/>
+        
+        <h2> 1.2.2 </h2>
+        <p> This is a paragraph </p>
+        <br/>
+        
+        <h2> 1.2.3 </h2>
+        <p> Link below </p>
+        <a href="https://www.youtube.com/"> Youtube </a>
+        <!-- image below-->
+        <p> Image below </p>
+        <img 
+            src="https://earthsky.org/upl/2019/04/bluejay-e1554247141817.jpg"  
+            title="Blue bird" 
+            alt="A Blue Bird"/> 
+        <p> A Blue Bird </p>
+        <hr/>
+        
+        
+        <h1> 1.5 </h1>
+        <h2> 1.5.1 </h2>
+        <table border="1">
+            <tr>
+                <td> Ô thứ nhất hàng 1 </td>
+                <td> Ô thứ hai hàng 1 </td>
+            </tr>
+            <tr>
+                <td> Ô thứ nhất hàng 2 </td>
+                <td> Ô thứ hai hàng 2 </td>
+            </tr>
+        </table>
+        <br/>
+        
+        <h2>1.5.3</h2>
+        <table border="1">
+            <tr>
+                <th> Đề mục </th>
+                <th> Đề mục 2 </th>
+            </tr>
+            <tr>
+                <td> Ô thứ nhất hàng 1 </td>
+                <td> Ô thứ hai hàng 1 </td>
+            </tr>
+            <tr>
+                <td> Ô thứ nhất hàng 2 </td>
+                <td> Ô thứ hai hàng 2 </td>
+            </tr>
+        </table>
+        
+        <br/>
+        <h2>1.5.3</h2>
+        <table border="1">
+            <tr>
+                <td> Ô thứ nhất hàng 1 </td>
+                <td> Ô thứ hai hàng 1 </td>
+            </tr>
+            <tr>
+                <td> Ô thứ nhất hàng 2 </td>
+                <td> &nbsp; </td>
+            </tr>
+        </table>
+        
+        <h1>1.6</h1>
+        <p style="
+            color: blue;
+            font-style: italic;
+            font-size: x-large">Text Style</p>
+        
+        <br/>
+        
+        <h1>1.7</h1>
+        <h2>1.7.1</h2>
+        <ul>
+            <li> Item 1 </li>
+            <li> Item 2 </li>
+        </ul>
+        <h2>1.7.2</h2>
+        <ol>
+            <li> Item 1</li>
+            <li> Item 2 </li>
+        </ol>
+        
+        
+    </body>
+        
+    
+
